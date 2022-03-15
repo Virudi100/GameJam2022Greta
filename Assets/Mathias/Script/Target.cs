aa6bf604c4f91b8f835847225f3408928000d30c
AlphaSaitam <97667455+AlphaSaitam@users.noreply.github.com>
2022-03-15 17:07:59 +0100

    pushinforprefabrotate

--- Assets/Mathias/Script/Target.cs
+++ Assets/Mathias/Script/Target.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
-    private float speedrotate = 2f;
-    void Start()
+    private float speedrotate = 500f;
+    void Update()
     {
-        
+        rotation();
     }
 
     // Update is called once per frame
-    void Update()
+    private void rotation()
     {
-        
+        gameObject.transform.Rotate(0, 1 * speedrotate * Time.deltaTime, 0); 
     }
 }
