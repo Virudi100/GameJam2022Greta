1c9af1c3f9e616f3655130ab925941c7274e8831
Virudi100 <77632799+Virudi100@users.noreply.github.com>
2022-03-15 13:03:21 +0100

    ia update

--- Assets/Anthony/Scripts/IAEnemi.cs
+++ Assets/Anthony/Scripts/IAEnemi.cs
@@ -7,16 +7,35 @@ public class IAEnemi : MonoBehaviour
 {
     private NavMeshAgent navmesh;
     [SerializeField] private GameObject player;
+    private bool isAggressive = true;
 
     // Start is called before the first frame update
     void Start()
     {
         navmesh = GetComponent<NavMeshAgent>();
+        StartCoroutine(Attack());
     }
 
     // Update is called once per frame
     void Update()
     {
-        navmesh.SetDestination(player.transform.position);
+        if (isAggressive == true)
+        {
+            navmesh.SetDestination(player.transform.position);
+        }
+        else
+            navmesh.SetDestination(-player.transform.position);
+        
     }
+
+    IEnumerator Attack()
+    {
+        yield return new WaitForSeconds(15);
+        isAggressive = false;
+        navmesh.speed = 40;
+        yield return new WaitForSeconds(10);
+        Destroy(gameObject);
+    }
+
+
 }
