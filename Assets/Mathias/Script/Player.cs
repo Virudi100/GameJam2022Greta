ca1c6939e0f4cb47486cd9271c380cc38902a170
AlphaSaitam <97667455+AlphaSaitam@users.noreply.github.com>
2022-03-15 10:37:20 +0100

    Movement

---
+++ Assets/Mathias/Script/Player.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 100f;
+    public float jump = 100f;
+    public Rigidbody rb;
+    
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+   
+    {
+        if (Input.GetKey(KeyCode.Z))
+        {
+           rb.AddForce(new Vector3(0,0,speed*Time.deltaTime));
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(new Vector3(0,0,-speed*Time.deltaTime));
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            rb.AddForce(new Vector3(speed*Time.deltaTime,0,0));
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(new Vector3(-speed*Time.deltaTime,0,0));
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+           rb.AddForce(new Vector3(0,jump*Time.deltaTime,0));
+        }
+            
+        
+    }
+}
