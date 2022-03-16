2a84e8917fd65bd2a82dac14380dce1be1882cbd
Virudi100 <77632799+Virudi100@users.noreply.github.com>
2022-03-16 16:40:58 +0100

    Music boss scene

--- Assets/Anthony/Scripts/EndGame.cs
+++ Assets/Anthony/Scripts/EndGame.cs
@@ -6,16 +6,23 @@ using UnityEngine.SceneManagement;
 public class EndGame : MonoBehaviour
 {
     [SerializeField] private GameObject winCanvas;
+    [SerializeField] private AudioSource cam;
+    [SerializeField] private AudioClip victory;
+    [SerializeField] private AudioClip bossMusic;
 
     private void Start()
     {
+        cam.clip = bossMusic;
         winCanvas.SetActive(false);
+        cam.Play();
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            cam.clip = victory;
+            cam.Play();
             winCanvas.SetActive(true);
             Time.timeScale = 0;
         }
